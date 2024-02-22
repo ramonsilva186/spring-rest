6b374ae19b2c1e6dfc3e4da8f537ae00390369ec
ramonsilva186 <ramonsilva186@gmail.com>
2024-02-22 15:44:47 -0300

    Project Created

---
+++ src/main/java/br/com/springrest/repository/ProdutoRepository.java
@@ -0,0 +1,7 @@
+package br.com.springrest.repository;
+
+import br.com.springrest.entity.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public class ProdutoRepository {
+}
